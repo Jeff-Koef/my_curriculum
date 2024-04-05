6ea8b5dd5d85eca0865095c7141e73fe57180f48
MaggieOBrienn <maggob12@gmail.com>
2024-04-05 16:39:00 -0400

    4/5 4:40

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function displayClasses() {
 }
 
 document.addEventListener('DOMContentLoaded', displayClasses);
+
+function openPage(pageName, evt) {
+    // Declare all variables
+    var i, buttoncontent, buttonlinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    buttoncontent = document.getElementsByClassName("buttoncontent");
+    for (i = 0; i < buttoncontent.length; i++) {
+      buttoncontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    buttonlinks = document.getElementsByClassName("buttonlinks");
+    for (i = 0; i < buttonlinks.length; i++) {
+      buttonlinks[i].className = buttonlinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
